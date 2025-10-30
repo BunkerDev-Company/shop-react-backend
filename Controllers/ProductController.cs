0d48da944db96e69e879c52baf2f13a0e5bdd1af
BunkerDev-Company <bunkerdev@yandex.ru>
2025-10-30 19:53:47 +0500

    ver 1.0

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopBackend.Helpers;
+
+namespace ShopBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("One")]
+        public IActionResult GetOneProducts(int id)
+        {
+            var product = FakeDatabase._products.Where(x=>x.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                return Ok("Товара нет");
+            }
+            return Ok(product);
+        }
+
+        [HttpGet("All")]
+        public IActionResult GetAllProduct()
+        {
+            var listProducts = FakeDatabase._products.ToList();
+            return Ok(listProducts);
+        }
+    }
+}
---
+++ Helpers/FakeDatabase.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml.Linq;
+
+namespace ShopBackend.Helpers
+{
+    public static class FakeDatabase
+    {
+        public static List<Product> _products = new List<Product>()
+        {
+            new Product()
+            {
+                Id = 1,
+                Name = "Кепка BunkerDev 2",
+                Price = 900
+            },
+            new Product()
+            {
+                Id = 2,
+                Name = "Худи Limited",
+                Price = 1700
+            },
+            new Product()
+            {
+                Id = 3,
+                Name = "Кроссовки Limited",
+                Price = 10000
+            },
+            new Product()
+            {
+                Id = 4,
+                Name = "Штаны Limited",
+                Price = 3000
+            }
+        };
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Price { get; set; }
+    }
+}
